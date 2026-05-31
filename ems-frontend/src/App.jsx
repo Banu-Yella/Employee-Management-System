44320a4e7dc075424d79b1dae97a540d94e7fdef
codes-by-saniya <saniyasaashaikh7884@gmail.com>
2026-05-31 21:59:23 +0530

    Added React Router and initial pages

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+
 function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
